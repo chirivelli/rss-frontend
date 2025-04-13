fbd908c375b91b3453609a7c694fa423d0c30e12
Sathwik <sathwikchirivelli@gmail.com>
2025-04-13 19:39:07 +0530

    tapping on snippet, reveals more of it

--- src/components/ArticleContent.jsx
+++ src/components/ArticleContent.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
+
 export default function ArticleContent({ htmlString }) {
+    const [enlarge, setEnlarge] = useState(false)
     return (
         <p
-            className='line-clamp-2 text-gray-200'
+            className={`${!enlarge && 'line-clamp-1'} text-gray-200`}
+            onClick={() => setEnlarge(prev => !prev)}
             dangerouslySetInnerHTML={{
                 __html: htmlString
                     .replaceAll('iframe', 'if')
