82add2a6645f41b0131caf77723f05c3a2bcc628
Sathwik <sathwikchirivelli@gmail.com>
2025-06-22 19:50:08 +0530

    Updated Account page

--- src/routes/Account.tsx
+++ src/routes/Account.tsx
@@ -1,8 +1,23 @@
 export default function Account() {
-  return (
-    <div>
-      <h1>Account</h1>
-      <p>This is the account page.</p>
-    </div>
-  );
+    return (
+        <div className='grid gap-4 rounded bg-slate-700 p-4'>
+            <div className='grid grid-cols-[200px_1fr] items-center'>
+                <label className='font-semibold text-slate-200'>Email</label>
+                <input
+                    className='rounded bg-slate-600 px-2 py-1'
+                    disabled
+                    value={'sathwikchirivelli@gmail.com'}
+                />
+            </div>
+
+            <div className='grid grid-cols-[200px_1fr] items-center'>
+                <label className='font-semibold text-slate-200'>Name</label>
+                <input
+                    className='rounded bg-slate-600 px-2 py-1'
+                    disabled
+                    value={'Sathwik Chirivelli'}
+                />
+            </div>
+        </div>
+    )
 }
