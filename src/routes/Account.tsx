7d4c641820bb79b93edc4b7118691c20e9bfdd44
Sathwik <sathwikchirivelli@gmail.com>
2025-06-24 22:55:22 +0530

    query key bug fix

--- src/routes/Account.tsx
+++ src/routes/Account.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 
 export default function Account() {
     const { data } = useQuery({
-        queryKey: ['newsfeed'],
+        queryKey: ['user'],
         queryFn: getUser,
     })
 
