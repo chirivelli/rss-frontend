ab9d4f6dd6dfb92e313705cf70144e54292905ad
Sathwik <sathwikchirivelli@gmail.com>
2025-07-07 21:42:18 +0530

    Added ClerkProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from '@/App'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const queryClient = new QueryClient()
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+}
+
 createRoot(document.getElementById('root')!).render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </ClerkProvider>,
 )
