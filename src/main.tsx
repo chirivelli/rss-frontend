79a47b75fab9474557dbaac4f6f9b5e0cbe1d985
Sathwik <sathwikchirivelli@gmail.com>
2025-08-18 22:05:47 +0530

    Added Auth via supabase

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from '@/App'
-import { ClerkProvider } from '@clerk/clerk-react'
 
 const queryClient = new QueryClient()
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-    throw new Error('Missing Publishable Key')
-}
-
 createRoot(document.getElementById('root')!).render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </ClerkProvider>,
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>,
 )
